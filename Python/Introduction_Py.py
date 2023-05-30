549ec10002afd2e40c4d75253cea57155683e7f3
wilman <130717547+Wiilmar@users.noreply.github.com>
2023-05-30 10:53:57 -0500

    Se realizaron algunos ejercicios en el archivo description

--- Python/Introduction_Py.py
+++ Python/Introduction_Py.py
@@ -4,8 +4,32 @@
 sum = 1 + 2
 print('La suma de 1 + 2 es:', sum, '(expresiva)')
 
-#Esto es uan suma declarativa
+#Esto es una suma declarativa
 Num1 = 2
 Num2 = 2
 Resultado = (Num1 + Num2)
+#El comando print me sirve para imprimir en pantalla
 print('La suma de', Num1, '+', Num2, 'es', Resultado, '(declarativa)')
+
+Cadena = 'Esto es una cadena'
+Entero = 1 #Esto es Int
+Float = 1.2 #Esto es un float
+Boleano = True #Esto es un Boleanoo
+print(Boleano)
+
+#operadores de signo
+valor = 2
+#Al usar el operador '+=' la variable 'valor' incrementara en 2.
+valor += 2
+#Al usar el operador '-=' la variable 'valor' se reduce en 2.
+valor -= 2
+#Al usar el operador '*=' la variable 'valor' se multiplica por 5.
+valor *= 5
+print(valor)
+
+#conversion de tipo de datos
+numero = 32254492
+nombre1 = 'Wilman'
+# Debo convertir ciertas variables a string para poder usar el siguiente comando:
+#print('Hola' + nombre1 + 'Tu numero es:' + numero)
+print('Hola ' + nombre1 + ' Tu numero es: ' + str(numero))
