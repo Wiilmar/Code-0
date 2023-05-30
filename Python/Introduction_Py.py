be53b12007226e2cf61a425f664f14867e66b36d
wilman <130717547+Wiilmar@users.noreply.github.com>
2023-05-30 10:23:14 -0500

    Se implemento un nuevo archivo llamado: Introduction.py

---
+++ Python/Introduction_Py.py
@@ -0,0 +1,11 @@
+# Para comentar en linea debo usar la tecla '#'
+
+# Esto es una suma expresiva
+sum = 1 + 2
+print('La suma de 1 + 2 es:', sum, '(expresiva)')
+
+#Esto es uan suma declarativa
+Num1 = 2
+Num2 = 2
+Resultado = (Num1 + Num2)
+print('La suma de', Num1, '+', Num2, 'es', Resultado, '(declarativa)')
