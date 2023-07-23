17cc5cb83dbef8a18e9b2ab3ad69dd8f87a0fe12
wilman <130717547+Wiilmar@users.noreply.github.com>
2023-07-23 17:42:44 -0500

    Cambios en el archivo induccion.js

---
+++ Lenguajes_Programacion/JavaScript/Aprendiendo_Js_FreeCodeCamp/introduccion.js
@@ -0,0 +1,45 @@
+console.log('Hola \nMundo');
+// Comentario en linea 
+/* Comentario en Bloque*/
+// - - - - - - - - - - - - - -
+let a;  //Variable definida pero no inicializada 
+let b = 2; //Variable definida y inicializada 
+console.log(a,b);
+// - - - - - - - - - - - - - -
+a = 3 / 0;
+console.log(a);
+// - - - - - - - - - - - - - -
+a = 5;
+a +=5;
+a -=2;
+a++;
+a--;
+console.log(a);
+// - - - - - - - - - - - - - -
+a = 'Soy una cadena de caracteres con "comillas" '; //Forma #1
+console.log(a);
+a = 'Te voy a pagar \"\$50.000\" '
+console.log(a); //Forma #2
+// - - - - - - - - - - - - - -
+// Secuencias de escape
+a = 'Hola \tmundo' //Me realiza un espacio entre las palabras
+console.log(a);
+a = 'Hola \fmundo';
+console.log(a);
+// - - - - - - - - - - - - - -
+//Concadenar cadenas
+a = 'WILMAN';
+b = ' Rodriguez';
+console.log(a+b); //Forma uno
+a= 'Wilman' + ' ' +'Rodriguez';
+console.log(a); //Forma dos
+a = 'Estoy aprendiendo'
+b = ' a programar en Js'
+a += b
+console.log(a)
+// - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - -
+// - - - - - - - - - - - - - -
